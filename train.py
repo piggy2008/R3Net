df4c79dca2913d6043dee4d5b38267130209e925
piggy2008 <piggy2008@example.com>
2019-04-25 11:08:19 +0100

    finetune model

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ import time
 from utils import load_part_of_model
 
 cudnn.benchmark = True
-device_id = 0
+device_id = 2
 torch.manual_seed(2019)
 torch.cuda.set_device(device_id)
 
@@ -32,12 +32,12 @@ args = {
     'iter_save': 5000,
     'train_batch_size': 1,
     'last_iter': 0,
-    'lr': 1e-6,
+    'lr': 1e-7,
     'lr_decay': 0.9,
     'weight_decay': 5e-4,
     'momentum': 0.9,
     'snapshot': '',
-    'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2019-04-24 23:34:00', '5000.pth'),
+    'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2019-04-25 00:54:41', '30000.pth'),
     # 'pretrain': '',
     # 'imgs_file': 'Pre-train/pretrain_all_seq2.txt',
     'imgs_file': 'video_saliency/train_all_DAFB3_seq_5f.txt',

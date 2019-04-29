8ccf2801892860ae6082565ff0277f7f2d4d4198
piggy2008 <piggy2008@example.com>
2019-04-29 11:24:22 +0100

    frozen layers

--- train_prior.py
+++ train_prior.py
@@ -28,7 +28,7 @@ exp_name = 'VideoSaliency' + '_' + time_str
 
 args = {
     'motion': 'GRU',
-    'iter_num': 30000,
+    'iter_num': 20000,
     'iter_save': 10000,
     'train_batch_size': 1,
     'last_iter': 0,
@@ -90,7 +90,7 @@ def fix_parameters(parameters):
 def main():
     net = R3Net_prior(motion=args['motion']).cuda().train()
 
-    fix_parameters(net.named_parameters())
+    # fix_parameters(net.named_parameters())
     optimizer = optim.SGD([
         {'params': [param for name, param in net.named_parameters() if name[-4:] == 'bias'],
          'lr': 2 * args['lr']},

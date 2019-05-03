6015ad76138dc2681166011347436b06aa5c9dc9
piggy2008 <piggy2008@example.com>
2019-05-03 10:41:08 +0100

    update batch_size:5

--- train_step.py
+++ train_step.py
@@ -30,7 +30,7 @@ args = {
     'motion': '',
     'iter_num': 30000,
     'iter_save': 10000,
-    'train_batch_size': 7,
+    'train_batch_size': 5,
     'last_iter': 0,
     'lr': 1e-3,
     'lr_decay': 0.9,

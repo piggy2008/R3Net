3d601c99ec7f563f847698bb7c2dd875dc5be632
piggy2008 <piggy2008@example.com>
2019-06-26 16:54:37 +0100

    testing

--- infer_prior_attention.py
+++ infer_prior_attention.py
@@ -14,15 +14,15 @@ from model_prior_attention import R3Net_prior
 torch.manual_seed(2018)
 
 # set which gpu to use
-torch.cuda.set_device(1)
+torch.cuda.set_device(0)
 
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
-exp_name = 'VideoSaliency_2019-06-26 18:08:11'
+exp_name = 'VideoSaliency_2019-06-26 18:42:54'
 
 args = {
-    'snapshot': '30000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (473, 473)

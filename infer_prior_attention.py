a49107faffeb9c838827230d7bb9b586ca84bedc
piggy2008 <piggy2008@example.com>
2019-06-26 16:22:58 +0100

    testing

--- infer_prior_attention.py
+++ infer_prior_attention.py
@@ -14,15 +14,15 @@ from model_prior_attention import R3Net_prior
 torch.manual_seed(2018)
 
 # set which gpu to use
-torch.cuda.set_device(0)
+torch.cuda.set_device(1)
 
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
-exp_name = 'VideoSaliency_2019-05-25 00:42:44'
+exp_name = 'VideoSaliency_2019-06-26 18:08:11'
 
 args = {
-    'snapshot': '30000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (473, 473)
@@ -63,7 +63,7 @@ imgs_path = os.path.join(davis_path, 'davis_test2_5f.txt')
 # imgs_path = os.path.join(segtrack_path, 'SegTrackV2_test_5f.txt')
 
 def main():
-    net = R3Net_prior(motion='GRU', se_layer=False, attention=True, pre_attention=True)
+    net = R3Net_prior(motion='GRU', se_layer=False, attention=True, pre_attention=True, basic_model='resnext50')
 
     print ('load snapshot \'%s\' for testing' % args['snapshot'])
     net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth'), map_location='cuda:0'))

a8f0a015fcf95600a6c93b948f5fff6df77f2ab6
piggy2008 <piggy2008@example.com>
2019-08-08 15:45:09 +0100

    add saliency map normalize

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import torch
 import os
 from model import R3Net
 
+def MaxMinNormalization(x,Max,Min):
+    x = (x - Min) / (Max - Min);
+    return x;
+
 def load_part_of_model(new_model, src_model_path, device_id=0):
     src_model = torch.load(src_model_path, map_location='cuda:' + str(device_id))
     m_dict = new_model.state_dict()
@@ -15,7 +19,7 @@ def load_part_of_model(new_model, src_model_path, device_id=0):
 
 if __name__ == '__main__':
     ckpt_path = './ckpt'
-    exp_name = 'VideoSaliency_2019-04-23 23:16:12'
+    exp_name = 'VideoSaliency_2019-05-14 17:13:16'
 
     args = {
         'snapshot': '30000',  # your snapshot filename (exclude extension name)

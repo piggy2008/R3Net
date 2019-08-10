2d8be95f014d4efcb94ecada53e44a4d15d35563
piggy2008 <piggy2008@example.com>
2019-08-10 22:19:24 +0100

    update module name

--- module/spa_module.py
+++ module/spa_module.py
@@ -35,3 +35,49 @@ class PAM_Module(nn.Module):
 
         out = self.gamma*out + x
         return out
+
+class STA_Module(nn.Module):
+    """ Position attention module"""
+    #Ref from SAGAN
+    def __init__(self, in_dim):
+        super(STA_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv_spatial = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.value_conv_temporal = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.gamma2 = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x_spatial, x_temporal):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        m_batchsize, C, height, width = x_spatial.size()
+        proj_query = self.query_conv(x_spatial).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(x_temporal).view(m_batchsize, -1, width*height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = F.softmax(energy, dim=-1)
+
+        proj_value = self.value_conv_spatial(x_spatial).view(m_batchsize, -1, width*height)
+        proj_value2 = self.value_conv_temporal(x_temporal).view(m_batchsize, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out2 = torch.bmm(proj_value2, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+        out2 = out2.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x_temporal + self.gamma2*out2 + x_temporal
+        return out
+
+if __name__ == '__main__':
+    x = torch.rand([5, 64, 70, 70])
+    y = torch.rand([5, 64, 70, 70])
+    model = STA_Module(in_dim=64)
+    out = model(x, y)
+    print(out.size())

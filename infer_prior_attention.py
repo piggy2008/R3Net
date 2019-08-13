48919493b473a6cb4ecc1db243d7bf4e9c4ebc3f
piggy2008 <piggy2008@example.com>
2019-08-13 20:01:19 +0100

    testing sta module with conv2d fuse

--- infer_prior_attention.py
+++ infer_prior_attention.py
@@ -23,7 +23,7 @@ ckpt_path = './ckpt'
 exp_name = 'VideoSaliency_2019-08-13 19:08:26'
 
 args = {
-    'snapshot': '20000',  # your snapshot filename (exclude extension name)
+    'snapshot': '30000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (473, 473)

702db39dd840d7ab646cb9202f3913ab94118e41
piggy2008 <piggy2008@example.com>
2019-08-13 15:45:39 +0100

    testing resnet50 no MEN

--- infer.py
+++ infer.py
@@ -6,9 +6,11 @@ from PIL import Image
 from torch.autograd import Variable
 from torchvision import transforms
 
-from config import ecssd_path, hkuis_path, pascals_path, sod_path, dutomron_path, davis_path
+from config import ecssd_path, hkuis_path, pascals_path, sod_path, dutomron_path, \
+    davis_path, fbms_path, mcl_path, uvsd_path, visal_path, vos_path, segtrack_path
 from misc import check_mkdir, crf_refine, AvgMeter, cal_precision_recall_mae, cal_fmeasure
 from model import R3Net
+from utils import MaxMinNormalization
 
 torch.manual_seed(2018)
 
@@ -18,10 +20,10 @@ torch.cuda.set_device(0)
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
-exp_name = 'VideoSaliency_2019-05-14 17:26:54'
+exp_name = 'VideoSaliency_2019-07-01 23:51:39'
 
 args = {
-    'snapshot': '30000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (473, 473)
@@ -35,11 +37,33 @@ to_pil = transforms.ToPILImage()
 
 # to_test = {'ecssd': ecssd_path, 'hkuis': hkuis_path, 'pascal': pascals_path, 'sod': sod_path, 'dutomron': dutomron_path}
 # to_test = {'ecssd': ecssd_path}
+
 to_test = {'davis': os.path.join(davis_path, 'davis_test2')}
 gt_root = os.path.join(davis_path, 'GT')
 imgs_path = os.path.join(davis_path, 'davis_test2_single.txt')
+
+# to_test = {'FBMS': os.path.join(fbms_path, 'FBMS_Testset')}
+# gt_root = os.path.join(fbms_path, 'GT')
+# imgs_path = os.path.join(fbms_path, 'FBMS_test_single.txt')
+
+# to_test = {'SegTrackV2': os.path.join(segtrack_path, 'SegTrackV2_test')}
+# gt_root = os.path.join(segtrack_path, 'GT')
+# imgs_path = os.path.join(segtrack_path, 'SegTrackV2_test_single.txt')
+
+# to_test = {'ViSal': os.path.join(visal_path, 'ViSal_test')}
+# gt_root = os.path.join(visal_path, 'GT')
+# imgs_path = os.path.join(visal_path, 'ViSal_test_single.txt')
+
+# to_test = {'VOS': os.path.join(vos_path, 'VOS_test')}
+# gt_root = os.path.join(vos_path, 'GT')
+# imgs_path = os.path.join(vos_path, 'VOS_test_single.txt')
+
+# to_test = {'MCL': os.path.join(mcl_path, 'MCL_test')}
+# gt_root = os.path.join(mcl_path, 'GT')
+# imgs_path = os.path.join(mcl_path, 'MCL_test_single.txt')
+
 def main():
-    net = R3Net(motion='', se_layer=False, attention=True)
+    net = R3Net(motion='', se_layer=False, attention=True, basic_model='resnet50')
 
     print ('load snapshot \'%s\' for testing' % args['snapshot'])
     net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth'), map_location='cuda:0'))
@@ -61,7 +85,10 @@ def main():
             for idx, img_name in enumerate(img_list):
                 print ('predicting for %s: %d / %d' % (name, idx + 1, len(img_list)))
 
-                img = Image.open(os.path.join(root, img_name + '.jpg')).convert('RGB')
+                if name == 'VOS':
+                    img = Image.open(os.path.join(root, img_name + '.png')).convert('RGB')
+                else:
+                    img = Image.open(os.path.join(root, img_name + '.jpg')).convert('RGB')
                 shape = img.size
                 img = img.resize(args['input_size'])
                 img_var = Variable(img_transform(img).unsqueeze(0), volatile=True).cuda()
@@ -69,7 +96,9 @@ def main():
                 precision = to_pil(prediction.data.squeeze(0).cpu())
                 precision = precision.resize(shape)
                 prediction = np.array(precision)
-
+                prediction = prediction.astype('float')
+                prediction = MaxMinNormalization(prediction, prediction.max(), prediction.min()) * 255.0
+                prediction = prediction.astype('uint8')
                 if args['crf_refine']:
                     prediction = crf_refine(np.array(img), prediction)
 

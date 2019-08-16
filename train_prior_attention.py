f07210bd3ed7c187038716838620cc0f1789e362
piggy2008 <piggy2008@example.com>
2019-08-16 23:03:32 +0100

    finetune sta with sequence data

--- train_prior_attention.py
+++ train_prior_attention.py
@@ -38,7 +38,7 @@ args = {
     'sta': True,
     'iter_num': 10000,
     'iter_save': 2500,
-    'train_batch_size': 1,
+    'train_batch_size': 5,
     'last_iter': 0,
     'lr': 1e-10,
     'lr_decay': 0.95,

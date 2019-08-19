72d41c413fb447f2c4c11d8fc50a6adad446e821
piggy2008 <piggy2008@example.com>
2019-08-19 16:38:37 +0100

    testing original resnet101

--- infer.py
+++ infer.py
@@ -23,7 +23,7 @@ ckpt_path = './ckpt'
 exp_name = 'VideoSaliency_2019-08-19 18:07:15'
 
 args = {
-    'snapshot': '20000',  # your snapshot filename (exclude extension name)
+    'snapshot': '15000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (473, 473)

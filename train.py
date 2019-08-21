364f25cfd9862531db55cd4281d1dbe5f2016aa5
piggy2008 <piggy2008@example.com>
2019-08-21 10:56:33 +0100

    testing original resnext101

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ args = {
     'snapshot': '',
     # 'pretrain': os.path.join(ckpt_path, 'VideoSaliency_2019-04-24 23:34:00', '10000.pth'),
     'pretrain': '',
-    'imgs_file': 'Pre-train/pretrain_all_seq_DUT_TR_DAFB2.txt',
+    'imgs_file': 'Pre-train/pretrain_all_seq_DUT_DAFB2.txt',
     # 'imgs_file': 'video_saliency/train_all_DAFB3_seq_5f.txt',
     'train_loader': 'video_image'
     # 'train_loader': 'video_sequence'
